ecc927ea402b29f9ec3a655d62700f657c8c5638
shahd-v <shahdfaisalr@gmail.com>
2026-02-23 11:00:05 +0000

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line using printf.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
