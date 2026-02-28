bc6e6f531b7b79b36d52b097140260fc9a19534a
shahd-v <shahdfaisalr@gmail.com>
2026-02-28 16:42:19 +0000

    Task 9: times_table function

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i * j;
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
+					_putchar(' ');
+			}
+
+			if (prod >= 10)
+			{
+				_putchar('0' + (prod / 10));
+				_putchar('0' + (prod % 10));
+			}
+			else
+				_putchar('0' + prod);
+		}
+		_putchar('\n');
+	}
+}
