e1a27dee82466aa847234e8dcdc1d3088eef3939
shahd-v <shahdfaisalr@gmail.com>
2026-03-06 23:01:12 +0000

    t2-betty fix 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,12 +4,11 @@
  * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
- * return: product of a and b
+ * Return: result  of a and b
  */
 
 int mul(int a, int b)
 {
 
-	int p = (a * b);
-	return (p);
+	return (a * b);
 }
