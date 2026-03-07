56552b9ac78e8469a10c8ec60d09b989a7e55351
root <root@ip-10-42-41-122.ec2.internal>
2026-03-07 20:27:21 +0000

    Add _strlen function that returns length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
+}
