12e884e91d09d29df30f0dc89b98442d5b0731df
root <root@ip-10-42-41-122.ec2.internal>
2026-03-07 20:53:30 +0000

    6-puts2: prints every other character correctly without extra chars

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: input string
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: the input string
  */
 void puts2(char *str)
 {
 	int i;
-
-	if (!str)
-		return;
 
 	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
