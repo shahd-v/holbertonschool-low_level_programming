ebbaa0c30ab94b8b9d456cc95a50d0623349e884
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 16:10:33 +0000

    t5.2

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers from 0 to 14, 11 times
+ * more_numbers - prints numbers from 0 to 14, 10 times
  *
  * You can only use _putchar three times per line
  */
@@ -9,16 +9,15 @@ void more_numbers(void)
 {
     int l, i;
 
-    for (l = 0; l < 11; l++)
+    for (l = 0; l < 10; l++)
     {
         for (i = 0; i <= 14; i++)
         {
             if (i >= 10)
-                _putchar('1');       // tens digit (only '1' because max is 14)
+                _putchar('1'); /* tens digit */
 
-            _putchar(i % 10 + '0');  // ones digit
-
+            _putchar(i % 10 + '0'); /* ones digit */
         }
-        _putchar('\n');              // third _putchar per line
+        _putchar('\n'); /* newline */
     }
 }
