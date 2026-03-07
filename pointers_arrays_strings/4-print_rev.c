8cf9dcad6c130fdfcf2c6127e900021c64ebc611
root <root@ip-10-42-41-122.ec2.internal>
2026-03-07 20:40:08 +0000

    4-print_rev: function that prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
