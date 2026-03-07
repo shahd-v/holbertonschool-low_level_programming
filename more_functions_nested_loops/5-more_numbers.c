f2af9c3f6348acdb5426590c9a3a6c09a108713f
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 16:14:33 +0000

    5.4

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 
 	for (l = 0; l < 10; l++)
 	{
-        for (i = 0; i <= 14; i++)
+		for (i = 0; i <= 14; i++)
 	{
 		if (i >= 10)
 		_putchar('1'); /* tens digit */
 
 		_putchar(i % 10 + '0'); /* ones digit */
-        }
+	}
 	_putchar('\n'); /* newline */
-    }
+	}
 }
