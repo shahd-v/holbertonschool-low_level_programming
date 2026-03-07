95ce09ec320640e261af21763f619c121777c7fd
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 14:18:58 +0000

    t4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+
+        int i;
+
+	while (i != 2 && i != 4)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(i + '0');
+		}
+	}
+	else
+	{
+	continue;
+	}
+
+        _putchar('\n');
+}
