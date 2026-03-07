4a4c8ee9162955486be39097ed18bef42070df34
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 16:48:24 +0000

    t7.1

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,21 +8,20 @@
  */
 void print_diagonal(int n)
 {
-    int i, j;
+	int i, j;
 
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
 
-    for (i = 0; i < n; i++)
-    {
-        /* print spaces before the backslash */
-        for (j = 0; j < i; j++)
-            _putchar(' ');
-
-        _putchar('\\'); /* print the diagonal character */
-        _putchar('\n'); /* move to next line */
-    }
+		_putchar('\\'); 
+		_putchar('\n'); /* move to next line */
+	}
 }
