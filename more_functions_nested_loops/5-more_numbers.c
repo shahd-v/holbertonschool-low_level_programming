c4a6354d9d960d1baf32eb03c51057c357e4c1b7
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 15:31:27 +0000

    t5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,17 @@ void more_numbers(void)
 	while (l < 11)
 	{
 		int i;
-		for (i = 0; i < 10; i++)
+		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + '0');
+			if (i < 10)
+			{
+				_putchar(i + '0');
+			}
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+			}
 		}
 		l++;
 		_putchar('\n');	
