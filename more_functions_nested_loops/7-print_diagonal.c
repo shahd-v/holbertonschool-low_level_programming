76b00bcd8cf5d94d4f0fffe3e578df12cffca8ef
shahd-v <shahdfaisalr@gmail.com>
2026-03-07 16:53:26 +0000

    t7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -14,12 +14,12 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 		return;
-	}	
+	}
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 			_putchar(' ');
-		_putchar('\\'); 
+		_putchar('\\');
 		_putchar('\n'); /* move to next line */
 	}
 }
