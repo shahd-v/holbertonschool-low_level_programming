12e61b91ca9f12292c1cb667b4c9c9d6ffba7ef4
root <root@ip-10-42-41-122.ec2.internal>
2026-03-07 20:52:21 +0000

    6-puts2: fixed to print every other character correctly without extra chars

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -2,16 +2,17 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: string to print
+ * @str: input string
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
-	{
+	if (!str)
+		return;
+
+	for (i = 0; str[i] != '\0'; i += 2)
 		_putchar(str[i]);
-		i += 2;
-	}
+
 	_putchar('\n');
 }
