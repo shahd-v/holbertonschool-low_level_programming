e0340d1dadfa415e5d4b47a817cf50d48bf37108
root <root@ip-10-42-41-122.ec2.internal>
2026-03-07 20:30:05 +0000

    Fix _puts implementation using _putchar

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -8,14 +8,10 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 
 	_putchar('\n');
