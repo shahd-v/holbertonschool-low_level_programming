c38eb2cd6850bc13d989ec7360f43de867257c1f
shahd-v <shahdfaisalr@gmail.com>
2026-03-10 12:04:27 +0000

    _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,21 +5,22 @@
  * @dest: input value
  * @src: input value
  *
- * Return: void
+ * Return: dest
  */
 
-void char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
+
 	int i = 0;
+	int j = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	int j = 0;
 	while (src[j] !='\0')
 	{
-		det[i] = src[j]
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
