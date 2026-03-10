894fa6840acc7d5d4568ccd7c99df8f3295a0c6b
root <root@ip-10-42-187-169.ec2.internal>
2026-03-10 11:54:53 +0000

    _strcat function

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+
+void char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	int j = 0;
+	while (src[j] !='\0')
+	{
+		det[i] = src[j]
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
