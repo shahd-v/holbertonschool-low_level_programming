a006fe96b78ac1f174432b409bc45e6e98637dc3
shahd-v <shahdfaisalr@gmail.com>
2026-03-10 13:17:52 +0000

    t6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -21,7 +21,7 @@ char *cap_string(char *str)
 
 			index++;
 
-		if (index == 0 || str[index - 1] == ' ' ||
+		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
 		    str[index - 1] == ',' ||
