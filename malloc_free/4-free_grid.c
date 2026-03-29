bdcca4aa369667ab99e6e0d9d8299f9d36140af7
root <root@ip-10-42-212-100.ec2.internal>
2026-03-29 14:51:34 +0000

    t:

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,41 +1,25 @@
-#include <stdio.h>
-		
-#include <stdlib.h>
-		
 #include "main.h"
-		
-/**
-		
- * free_grid - frees 2d array
-		
- * @grid: 2d grid
-		
- * @height: height dimension of grid
-		
- * Description: frees memory of grid
-		
- * Return: nothing
-		
- *
-		
- */
-		
-void free_grid(int **grid, int height)
-		
-{
-		
-	int i;
-		
+#include <stdlib.h>
 
-		
+/**
+ * free_grid - frees a 2D array of integers
+ * @grid: pointer to the 2D array
+ * @height: number of rows
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	/* Check for NULL pointer */
+	if (grid == NULL)
+		return;
+
+	/* Free each row */
 	for (i = 0; i < height; i++)
-		
-	{
-		
 		free(grid[i]);
-		
-	}
-		
+
+	/* Free the main pointer */
 	free(grid);
-		
 }
