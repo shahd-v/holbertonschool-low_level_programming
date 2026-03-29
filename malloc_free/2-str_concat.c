c1ad78088aa2dd0400c8cd03c342f8574c254874
root <root@ip-10-42-212-100.ec2.internal>
2026-03-29 14:48:29 +0000

    t:

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,83 +1,49 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * str_concat - concatenates two strings into a newly allocated memory
+ * @s1: first string (can be NULL, treated as empty string)
+ * @s2: second string (can be NULL, treated as empty string)
+ *
+ * Return: pointer to the newly concatenated string,
+ *         or NULL if memory allocation fails
  */
-		
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+	char *result;
+	int len1 = 0, len2 = 0;
+	int i, j;
 
-		
+	/* Treat NULL as empty string */
 	if (s1 == NULL)
-		
 		s1 = "";
-		
 	if (s2 == NULL)
-		
 		s2 = "";
-		
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
+	/* Calculate length of s1 */
+	while (s1[len1] != '\0')
+		len1++;
 
-		
-	if (conct == NULL)
-		
+	/* Calculate length of s2 */
+	while (s2[len2] != '\0')
+		len2++;
+
+	/* Allocate memory for concatenated string */
+	result = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (result == NULL)
 		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
 
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+	/* Copy s1 into result */
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+
+	/* Copy s2 into result */
+	for (j = 0; j < len2; j++)
+		result[i + j] = s2[j];
+
+	/* Null-terminate the string */
+	result[len1 + len2] = '\0';
+
+	return (result);
 }
